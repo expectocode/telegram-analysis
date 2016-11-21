d2d69889f78b2adf4170519b60ef42b0d0f8704f
Tanuj <expectocode@users.noreply.github.com>
2016-11-21 23:10:44 +0000

    Updated phraseovertime with explanatory comments

--- phraseovertime.py
+++ phraseovertime.py
@@ -26,12 +26,12 @@ def main():
     savefolder = args.output_folder
 
     with open(filepath, 'r') as jsonfile:
-        events = (loads(line) for line in jsonfile)
-        counters = [defaultdict(list) for keyword in keywords]
+        events = (loads(line) for line in jsonfile) #generator, so whole file is not put in mem
+        counters = [defaultdict(list) for keyword in keywords] #list of defaultdicts. one dd per keyword
         for event in events:
-            for keyword in keywords:
-                if "text" in event:
-                    if args.case_sensitive:
+            if "text" in event: #if the event has the json tag "text" ie if it's a text message then search for given keywords. there is no else.
+                for keyword in keywords:
+                    if args.case_sensitive: #case sens or insens search for the keyword
                         if "text" in event:
                             day = date.fromtimestamp(event["date"])
                             counters[keywords.index(keyword)][day].append("text" in event and keyword in event["text"])
@@ -41,26 +41,34 @@ def main():
 
     _, filename = path.split(filepath)
     filename, _ = path.splitext(filename)
+    #make filename just the name of the file, with no leading directories and no extension
 
-#    frequencies = [{key: l.count(True)/l.count(False) * 100 for key, l in counter.items()}]
+#    frequencies = [{key: l.count(True)/l.count(False) * 100 for key, l in counter.items()}] #had to be moved to loop
     frequenciesList = []
-    plt.figure(figsize=(17,10))
-    for x in range(0, len(keywords)):
-        frequenciesList.append({key: l.count(True)/l.count(False) * 100 for key, l in counters[x].items()})
+    plt.figure(figsize=(14,8)) #make a decent default size.
+    for x in range(0, len(keywords)): #make a frequencies thing for each keyword, and plot each one onto the plot
+        frequenciesList.append({key: l.count(True)/l.count(False) * 100 for key, l in counters[x].items()}) #find frequency of keyword use per date
         plt.plot(*zip(*sorted(frequenciesList[x].items())))
+        plt.grid() #because i think it looks better with the grid
 
     plt.legend(keywords)
-    if args.case_sensitive:
+    if args.case_sensitive: #pretty self explanatory. this is added to the title.
         postfix=", case sensitive"
     else:
         postfix=", case insensitive"
-    plt.title("usage of {} in {}{}".format(keywords, filename, postfix)) #file name minus extension jsonl"
+    plt.title("usage of {} in {}{}".format(keywords, filename, postfix))
 
 #    plt.ylabel("Percentage of messages containing \"{}\"".format(keyword), size=14)
-    plt.ylabel("Percentage of messages containing keywords".format(keyword), size=14)
-    if savefolder is not None:
-        plt.savefig("{}/{}_in_{}.png".format(savefolder, keyword, filename))
-    else:
+    plt.ylabel("Percentage of messages containing keywords", size=14)
+    #not sure if it's really a percentage like it says, i get values over over 100?!?
+    if savefolder is not None: #if there is a given folder to save the figure in, save it there
+        keywords_string = ""
+        for keyword in keywords:
+            keywords_string+=keyword
+            keywords_string+="_"
+
+        plt.savefig("{}/{}_in_{}.png".format(savefolder, keywords_string, filename))
+    else: #if a save folder was not specified, just open a window to display graph
         plt.show()
 
 if __name__ == "__main__":
