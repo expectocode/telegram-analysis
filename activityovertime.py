e4367e34799cc8a43e75e9ce26b1c189ef9bce41
Tanuj <expectocode@users.noreply.github.com>
2016-11-23 22:24:43 +0000

    Made activity graph script take multiple chats as arguments

--- activityovertime.py
+++ activityovertime.py
@@ -15,35 +15,45 @@ def main():
     """
     parser = argparse.ArgumentParser(
             description="Visualise the activity of a chat over time")
-    parser.add_argument('filepath', help='path to the json file (chat log) to analyse')
     parser.add_argument('-o', '--output-folder', help='output the figure to image file in this folder')
+    parser.add_argument('filepaths', help='paths to the json file(s) (chat logs) to analyse. Note these must be at the end of the arguments.', nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
-    filepath = args.filepath
+    filepaths = args.filepaths
     savefolder = args.output_folder
 
-    with open(filepath, 'r') as jsonfile:
-        events = (loads(line) for line in jsonfile) #generator, so whole file is not put in mem
-        counter = defaultdict(int)  #a dict with dates as keys and frequency as values
-        for event in events:
-            day = date.fromtimestamp(event["date"])
-            counter[day] += 1
+    filenames = []
+    plt.figure(figsize=(14,8)) #make a decent default size.
 
-    _, filename = path.split(filepath)
-    filename, _ = path.splitext(filename)
+    for filepath in filepaths:
+        with open(filepath, 'r') as jsonfile:
+            events = (loads(line) for line in jsonfile) #generator, so whole file is not put in mem
+            counter = defaultdict(int)  #a dict with dates as keys and frequency as values
+            for event in events:
+                day = date.fromtimestamp(event["date"])
+                counter[day] += 1
+
+        _, temp = path.split(filepath)
+        filenames.append(temp)
+        filenames[filepaths.index(filepath)] , _ = path.splitext(filenames[filepaths.index(filepath)] )
     #make filename just the name of the file, with no leading directories and no extension
 
-    plt.figure(figsize=(14,8)) #make a decent default size.
-    frequencies = sorted(counter.items()) #find frequency of keyword use per date
-    plt.plot(*zip(*frequencies))
-    plt.grid() #because i think it looks better with the grid
+        frequencies = sorted(counter.items()) #find frequency of chat events per date
+        plt.plot(*zip(*frequencies))
+        plt.grid() #because i think it looks better with the grid
 
-    plt.title("Activity in {}".format(filename))
+    #end for
 
+    plt.title("Activity in {}".format(filenames))
+    plt.legend(filenames)
     plt.ylabel("Activity level (events per day)", size=14)
 
     if savefolder is not None: #if there is a given folder to save the figure in, save it there
-        plt.savefig("{}/activity_in_{}.png".format(savefolder, filename))
+        chats_string = ""
+        for chat in filenames:
+            chats_string+=chat
+            chats_string+="_"
+        plt.savefig("{}/activity_in_{}.png".format(savefolder, chats_string))
     else: #if a save folder was not specified, just open a window to display graph
         plt.show()
 
