f18a00db6b6913d63bdf8d3a2d8311aaebc8b4c2
Tanuj <expectocode@users.noreply.github.com>
2016-11-26 17:51:53 +0000

    Made scripts more consistent

--- activityovertime.py
+++ activityovertime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-A program to plot the popularity of a phrase in a chat over time
+A program to plot the activity in a chat over time
 """
 import argparse
 from json import loads
@@ -15,37 +15,54 @@ def main():
     """
     parser = argparse.ArgumentParser(
             description="Visualise the activity of a chat over time")
-    parser.add_argument('-o', '--output-folder', help='output the figure to image file in this folder')
-    parser.add_argument('filepaths', help='paths to the json file(s) (chat logs) to analyse. Note these must be at the end of the arguments.', nargs=argparse.REMAINDER)
+    parser.add_argument(
+            '-o', '--output-folder',
+            help='output the figure to image file in this folder')
+    parser.add_argument(
+            'filepaths',
+            help='paths to the json file(s) (chat logs) to analyse. Note these must be at the end of the arguments.',
+            nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
     filepaths = args.filepaths
     savefolder = args.output_folder
 
     filenames = []
-    plt.figure(figsize=(14,8)) #make a decent default size.
+    plt.figure(figsize=(14,8))
+    #make a decent default size.
 
     for filepath in filepaths:
         with open(filepath, 'r') as jsonfile:
-            events = (loads(line) for line in jsonfile) #generator, so whole file is not put in mem
-            counter = defaultdict(int)  #a dict with dates as keys and frequency as values
+            events = (loads(line) for line in jsonfile)
+            #generator, so whole file is not put in mem
+            counter = defaultdict(int)
+            #a dict with dates as keys and frequency as values
             for event in events:
                 day = date.fromtimestamp(event["date"])
                 counter[day] += 1
 
         _, temp = path.split(filepath)
         filenames.append(temp)
-        filenames[filepaths.index(filepath)] , _ = path.splitext(filenames[filepaths.index(filepath)] )
-    #make filename just the name of the file, with no leading directories and no extension
+        filenames[filepaths.index(filepath)] , _ = path.splitext(
+                filenames[filepaths.index(filepath)] )
+    #make filename just the name of the file,
+    # with no leading directories and no extension
 
-        frequencies = sorted(counter.items()) #find frequency of chat events per date
+        frequencies = sorted(counter.items())
+        #find frequency of chat events per date
+
         plt.plot(*zip(*frequencies))
-        plt.grid() #because i think it looks better with the grid
+        plt.grid()
+        #because i think it looks better with the grid
 
     #end for
 
-    plt.title("Activity in {}".format(filenames))
-    plt.legend(filenames)
+    if len(filenames) > 1:
+        plt.title("Activity in {}".format(filenames))
+        plt.legend(filenames, loc='best')
+    else:
+        plt.title("Activity in {}".format(filenames[0]))
+
     plt.ylabel("Activity level (events per day)", size=14)
 
     if savefolder is not None: #if there is a given folder to save the figure in, save it there
@@ -54,11 +71,16 @@ def main():
             chats_string+=chat
             chats_string+="_"
 
-        if len(chats_string) > 200:#file name likely to be so long as to cause issues
-            chats_string = input("This graph is going to have a very long file name. Please enter a custom name(no need to add an extension): ")
+        if len(chats_string) > 200:
+        #file name likely to be so long as to cause issues
+            figname = input(
+                "This graph is going to have a very long file name. Please enter a custom name(no need to add an extension): ")
+        else:
+            figname = "Activity in {}".format(chats_string)
 
-        plt.savefig("{}/activity_in_{}.png".format(savefolder, chats_string))
-    else: #if a save folder was not specified, just open a window to display graph
+        plt.savefig("{}/{}.png".format(savefolder, figname))
+    else:
+    #if a save folder was not specified, just open a window to display graph
         plt.show()
 
 if __name__ == "__main__":
--- phraseovertime.py
+++ phraseovertime.py
@@ -15,10 +15,16 @@ def main():
     """
     parser = argparse.ArgumentParser(
             description="Visualise the usage of a phrase in a chat over time")
-    parser.add_argument('filepath', help='path to the json file (chat log) to analyse')
-    parser.add_argument('-c', '--case-sensitive', help='make the phrase search case sensitive', action='store_true')
-    parser.add_argument('-o', '--output-folder', help='output the figure to image file in this folder')
-    parser.add_argument('keywords', help='the keyword(s) to search for (note that these must be at the end of the argument list)', nargs=argparse.REMAINDER)
+    parser.add_argument('filepath',
+            help='path to the json file (chat log) to analyse')
+    parser.add_argument('-c', '--case-sensitive',
+            help='make the phrase search case sensitive',
+            action='store_true')
+    parser.add_argument('-o', '--output-folder',
+            help='output the figure to image file in this folder')
+    parser.add_argument('keywords',
+            help='the keyword(s) to search for (note that these must be at the end of the argument list)',
+            nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
     filepath = args.filepath
@@ -26,48 +32,77 @@ def main():
     savefolder = args.output_folder
 
     with open(filepath, 'r') as jsonfile:
-        events = (loads(line) for line in jsonfile) #generator, so whole file is not put in mem
-        counters = [defaultdict(list) for keyword in keywords] #list of defaultdicts. one dd per keyword
+        events = (loads(line) for line in jsonfile)
+        #generator, so whole file is not put in mem
+        counters = [defaultdict(list) for keyword in keywords]
+        #list of defaultdicts. one dd per keyword
         for event in events:
-            if "text" in event: #if the event has the json tag "text" ie if it's a text message then search for given keywords. there is no else.
+            if "text" in event:
+            #if the event has the json tag "text" ie if it's a text message then search for given keywords. there is no else.
                 for keyword in keywords:
-                    if args.case_sensitive: #case sens or insens search for the keyword
+                    if args.case_sensitive:
+                    #case sens or insens search for the keyword
                         day = date.fromtimestamp(event["date"])
-                        counters[keywords.index(keyword)][day].append("text" in event and keyword in event["text"])
+                        counters[keywords.index(keyword)][day].append(
+                            "text" in event and keyword in event["text"])
                     else:
                         day = date.fromtimestamp(event["date"])
-                        counters[keywords.index(keyword)][day].append("text" in event and keyword in event["text"].lower())
+                        counters[keywords.index(keyword)][day].append(
+                            "text" in event and keyword in event["text"].lower())
 
     _, filename = path.split(filepath)
     filename, _ = path.splitext(filename)
-    #make filename just the name of the file, with no leading directories and no extension
+    #make filename just the name of the file,
+    # with no leading directories and no extension
 
 #    frequencies = [{key: l.count(True)/l.count(False) * 100 for key, l in counter.items()}] #had to be moved to loop
     frequenciesList = []
     plt.figure(figsize=(14,8)) #make a decent default size.
-    for x in range(0, len(keywords)): #make a frequencies thing for each keyword, and plot each one onto the plot
-        frequenciesList.append({key: l.count(True)/len(l) * 100 for key, l in counters[x].items()}) #find frequency of keyword use per date
+    for x in range(0, len(keywords)):
+    #make a frequencies thing for each keyword, and plot each one onto the plot
+        frequenciesList.append(
+        {key: l.count(True)/len(l) * 100 for key, l in counters[x].items()})
+
+        #find frequency of keyword use per date
         plt.plot(*zip(*sorted(frequenciesList[x].items())))
 
-    plt.grid() #because i think it looks better with the grid
+    plt.grid()
+    #because i think it looks better with the grid
 
-    plt.legend(keywords)
-    if args.case_sensitive: #pretty self explanatory. this is added to the title.
+    if args.case_sensitive:
+    #pretty self explanatory. this is added to the title.
         postfix=", case sensitive"
     else:
         postfix=", case insensitive"
-    plt.title("usage of {} in {}{}".format(keywords, filename, postfix))
+    if len(keywords) > 1:
+        plt.title(
+            "usage of {} in {}{}".format(keywords, filename, postfix))
+        plt.legend(keywords)
+    else:
+        plt.title(
+            "usage of {} in {}{}".format(keywords[0], filename, postfix))
+        #plt.legend(keywords[0])
 
 #    plt.ylabel("Percentage of messages containing \"{}\"".format(keyword), size=14)
     plt.ylabel("Percentage of messages containing keywords", size=14)
-    if savefolder is not None: #if there is a given folder to save the figure in, save it there
+    if savefolder is not None:
+    #if there is a given folder to save the figure in, save it there
         keywords_string = ""
         for keyword in keywords:
             keywords_string+=keyword
             keywords_string+="_"
 
-        plt.savefig("{}/{}_in_{}.png".format(savefolder, keywords_string, filename))
-    else: #if a save folder was not specified, just open a window to display graph
+        if len(keywords_string) > 200:
+        #file name likely to be so long as to cause issues
+            figname = input(
+                "This graph is going to have a very long file name. Please enter a custom name(no need to add an extension): ")
+        else:
+            figname = "{} in {}.png".format(
+                keywords_string, filename)
+
+        plt.savefig("{}/{}.png".format(savefolder, figname))
+    else:
+    #if a save folder was not specified, just open a window to display graph
         plt.show()
 
 if __name__ == "__main__":
