373dc79831a383da0f577a87b34e10836385c8da
Tanuj <expectocode@users.noreply.github.com>
2017-04-07 18:42:11 +0100

    Add inactive user count and user growth graph

---
+++ inactiveusers.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+A program to find a rough percentage of users in a chat who have sent less than 3 messages.
+"""
+import argparse
+from json import loads
+from os import path
+from collections import defaultdict
+
+def main():
+    """
+    main function
+    """
+    #cutoff for a 'non active' user
+    minimum = 3
+    non_active_users = 0
+    active_users = 0
+    parser = argparse.ArgumentParser(description="Find the number of non-active users (users who have sent less than 3 messages) in a chat, given a JSON log.")
+    parser.add_argument('filepath', help='the json file to analyse')
+
+    args = parser.parse_args()
+    filepath = args.filepath
+
+    _, filename = path.split(filepath)
+    filename, _ = path.splitext(filename)
+    #make filename just the name of the file, with no leading directories and no extension
+
+    counter = defaultdict(int) #store events from each user
+    #names = {} #dict
+    total_datapoints = 0
+
+    with open(filepath, 'r') as jsonfile:
+        events = (loads(line) for line in jsonfile)
+        for event in events:
+            if "from" in event:
+                if "peer_id" in event["from"] and "print_name" in event["from"]:
+                    total_datapoints += 1
+                    user = event['from']['peer_id']
+                    counter[user] += 1
+
+    trimmedCounter = defaultdict(int)
+
+    for person, frequency in counter.items():
+        if frequency < minimum:
+            #trimmedCounter["other"] += frequency
+            non_active_users += 1
+        else:
+            #trimmedCounter[names[str(person)]] = frequency
+            active_users += 1
+
+    print("For this chat, there were", non_active_users,
+        "users who sent less than", minimum, "messages, out of a total of",
+        non_active_users + active_users)
+    print("That's", round(100* non_active_users/(non_active_users + active_users),1), "%!")
+
+#    print(type(*sorted(counter.items())))
+ #   plt.pie(*zip(*sorted(counter.items())))
+
+if __name__ == "__main__":
+    main()
---
+++ usersovertime.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+A program to plot the users joining a chat over time. Note that leaving events are not noted.
+TODO: support multiple chats.
+"""
+import argparse
+import os
+from json import loads
+from datetime import date
+from collections import defaultdict
+from pprint import pprint
+
+import matplotlib.pyplot as plt
+
+def main():
+    """
+    main function
+    """
+    parser = argparse.ArgumentParser(
+            description="Visualise the growth a chat over time - note that leavers are not recorded, so this program can only show the join rate.")
+    parser.add_argument('path', help='the json file (chat log) to analyse')
+
+    args = parser.parse_args()
+    path = args.path
+
+    with open(path, 'r') as f:
+        events = (loads(line) for line in f)
+
+        counter = defaultdict(int)
+        for event in events:
+            if "action" in event and (event["action"]["type"] == "chat_add_user" or event['action']['type'] == 'chat_add_user_link'):
+                day = date.fromtimestamp(event["date"])
+                counter[day] += 1
+
+    _, filename = os.path.split(path)
+    filename, _ = os.path.splitext(filename)
+
+    #frequencies = {key: l.count(True)/l.count(False) * 100 for key, l in counter.items()}
+    users_per_day = sorted(counter.items())
+
+    u_count = 0
+    for idx, (day, users) in enumerate(users_per_day):
+        u_count += users
+        users_per_day[idx] = (day, u_count)
+
+    print(users_per_day)
+    plt.plot(*zip(*users_per_day))
+    plt.title('members in "{}"'.format(filename))
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
