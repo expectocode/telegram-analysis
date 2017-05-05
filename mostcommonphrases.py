60c3542c697347bda38bd6d44c8a06e7a6ea209c
Tanuj <expectocode@users.noreply.github.com>
2017-05-05 20:51:42 +0100

    Add args for bar chart

--- mostcommonphrases.py
+++ mostcommonphrases.py
@@ -1,34 +1,88 @@
 #!/usr/bin/env python3
-#a program to find the most common lines (messages) in an input (tg log)
+"""
+A program to find the most common lines in an input.
+Developed to find most common messages in a chatlog
+"""
 
 from sys import stdin
+import argparse
 import matplotlib.pyplot as plt
 from collections import Counter
 
 def main():
+    """
+    main function
+    """
 
-    number_of_phrases = 20
-    sortedfreqs = Counter(map(lambda x: x.rstrip().lower(),stdin)).most_common(3*number_of_phrases)
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description='Find the most commonly repeated lines and their frequencies from an input.',
+            epilog="""Example usage:
+Get all text from a chat and find the most repeated messages:
+    ./getalltext.py chat_name.jsonl | ./mostcommonphrases.py
+Find the most repeated lines in a textfile:
+    ./mostcommonphrases.py < textfile.txt
+Not recommended:
+    running this program with no arguments. This will wait for input and EOF."""
+    )
+    parser.add_argument(
+            '-g','--graph',
+            help='Create a bar chart showing the most common phrases',
+            action='store_true')
+    parser.add_argument('-o', '--output-folder',
+            help='the folder to save the bar chart image in.'
+            'Using this option will make the graph not display on screen.'
+            'This option has no effect if -g/--graph is not specified.')
+    parser.add_argument('-n','--number-of-phrases',
+            help='The number of phrases to print. Default 20.',
+            type=int)
+    parser.add_argument('-b','--number-of-bars',
+            help='The number of bars to show on the bar chart (cannot be larger than number of phrases). Default 20.',
+            type=int)
+    args = parser.parse_args()
+
+    number_of_phrases = args.number_of_phrases if (args.number_of_phrases is not None) else 20
+    if args.number_of_bars is not None:
+        #its defined, lets make sure its sane
+        if args.number_of_bars > number_of_phrases:
+            number_of_bars = number_of_phrases
+        else:
+            number_of_bars = args.number_of_bars
+    elif args.number_of_phrases is not None:
+        #its not defined, but if the other is let's make them equal
+        number_of_bars = number_of_phrases
+    else:
+        #its not defined and neither is the other.
+        number_of_bars = number_of_phrases
+
+    sortedfreqs = Counter(map(lambda x: x.rstrip().lower(),stdin)
+            ) .most_common(number_of_phrases)
+    #most common phrases from a counter object which takes all the lines from stdin and strips em
 
     #delete all with freq 1
     sortedfreqs  = [x for x in sortedfreqs if x[1] != 1]
 
     print(sortedfreqs) # output the list
-    #now just deal with the top 10% of phrases
-    sortedfreqs = sortedfreqs[:number_of_phrases]
-    #frequency_threshold = sortedfreqs[len(sortedfreqs)//5][1]
-    #sortedfreqs = [x for x in sortedfreqs if x[1] > frequency_threshold]
-    phrases,frequencies = list(zip(*sortedfreqs))
-    y_pos = range(len(phrases))
-    width = 0.6
-    plt.figure(figsize=(13,9))
-    plt.bar([x*2 for x in y_pos],frequencies,align='center',width=width)
-    plt.ylabel('frequency')
-    plt.title('most common phrases')
-    plt.xticks([x*2+width/3 for x in y_pos], phrases,rotation=25,ha='right')
-    #list comp makes the coords all shifted slightly, rotation is for readability,
-    #ha=right ensures thaht the rotated labels have their right side under the bar they refer to
-    plt.show()
+
+    if args.graph:
+
+        #now just deal with the top 10% of phrases
+        sortedfreqs = sortedfreqs[:number_of_bars]
+        #frequency_threshold = sortedfreqs[len(sortedfreqs)//5][1]
+        #sortedfreqs = [x for x in sortedfreqs if x[1] > frequency_threshold]
+        phrases,frequencies = list(zip(*sortedfreqs))
+        y_pos = range(len(phrases))
+        width = 0.6
+        plt.figure(figsize=(13,9))
+        plt.bar([x*2 for x in y_pos],frequencies,align='center',width=width)
+        plt.ylabel('frequency')
+        plt.title('most common phrases')
+        plt.xticks([x*2+width/3 for x in y_pos], phrases,rotation=25,ha='right')
+        #list comp makes the coords all shifted slightly, rotation is for readability,
+        #ha=right ensures thaht the rotated labels have their right side under the bar they refer to
+        plt.show()
+    else:
+        #show a warning message if they use a graph arg and theres no --graph
 
 if __name__ == "__main__":
     main()
