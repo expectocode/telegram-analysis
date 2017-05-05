4c56aea35f2dd326af5a77a73214c1107987044e
Tanuj <expectocode@users.noreply.github.com>
2017-05-05 18:34:58 +0100

    Massively improved performance of mostcommonphrases.py

--- mostcommonphrases.py
+++ mostcommonphrases.py
@@ -1,40 +1,34 @@
 #!/usr/bin/env python3
 #a program to find the most common lines (messages) in an input (tg log)
-#TODO:optimise this whole program
 
 from sys import stdin
+import matplotlib.pyplot as plt
+from collections import Counter
 
-#homemade way to find the index of the list containing a certain message inside a list of lists
-#probably horrible inefficient
-def where_in_list( phrase, listToSearch ):
-    for thing in listToSearch:
-        if phrase in thing:
-            return listToSearch.index(thing)
-        else:
-            continue
-    return -1
-    #if it hasn't found a match in the for
+def main():
 
-def getKey(item):
-    return item[1]
+    number_of_phrases = 20
+    sortedfreqs = Counter(map(lambda x: x.rstrip().lower(),stdin)).most_common(3*number_of_phrases)
 
-messagesFrequencyList = [["example phrase", 1]]
+    #delete all with freq 1
+    sortedfreqs  = [x for x in sortedfreqs if x[1] != 1]
 
-#print(type(messagesFrequencyList[0][0]))
-#print(messages[0][0])
+    print(sortedfreqs) # output the list
+    #now just deal with the top 10% of phrases
+    sortedfreqs = sortedfreqs[:number_of_phrases]
+    #frequency_threshold = sortedfreqs[len(sortedfreqs)//5][1]
+    #sortedfreqs = [x for x in sortedfreqs if x[1] > frequency_threshold]
+    phrases,frequencies = list(zip(*sortedfreqs))
+    y_pos = range(len(phrases))
+    width = 0.6
+    plt.figure(figsize=(13,9))
+    plt.bar([x*2 for x in y_pos],frequencies,align='center',width=width)
+    plt.ylabel('frequency')
+    plt.title('most common phrases')
+    plt.xticks([x*2+width/3 for x in y_pos], phrases,rotation=25,ha='right')
+    #list comp makes the coords all shifted slightly, rotation is for readability,
+    #ha=right ensures thaht the rotated labels have their right side under the bar they refer to
+    plt.show()
 
-for line in stdin:
-    message = line.rstrip().lower()
-    indexFound = where_in_list(message, messagesFrequencyList)
-    if indexFound > -1:
-        messagesFrequencyList[indexFound][1]+=1 #increase frequency
-    else:
-        messagesFrequencyList.append([message,1])
-
-#delete all with freq 1
-#go through the list of lists backwards. -1 is magic ;)
-for x in range(len(messagesFrequencyList) -1, -1, -1):
-    if messagesFrequencyList[x][1] == 1: #if freq is 1
-        del messagesFrequencyList[x]
-
-print(sorted(messagesFrequencyList, key=getKey, reverse=True))
+if __name__ == "__main__":
+    main()
