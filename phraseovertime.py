d1cd46326129e7d3341b959d850d0b011d18bfe4
Tanuj <expectocode@users.noreply.github.com>
2017-05-05 18:35:11 +0100

    Make graphs smoother by default - smoothness can be controlled with arg

--- phraseovertime.py
+++ phraseovertime.py
@@ -4,7 +4,7 @@ A program to plot the popularity of a phrase in a chat over time
 """
 import argparse
 from json import loads
-from datetime import date
+from datetime import date,timedelta
 from os import path
 from collections import defaultdict
 import matplotlib.pyplot as plt
@@ -14,41 +14,59 @@ def main():
     main function
     """
     parser = argparse.ArgumentParser(
-            description="Visualise the usage of a phrase in a chat over time")
-    parser.add_argument('filepath',
-            help='path to the json file (chat log) to analyse')
+            description="Visualise and compare the usage of one or more words/phrases in a chat over time")
+    required = parser.add_argument_group('required arguments')
     parser.add_argument('-c', '--case-sensitive',
             help='make the phrase search case sensitive',
             action='store_true')
     parser.add_argument('-o', '--output-folder',
             help='output the figure to image file in this folder')
-    parser.add_argument('keywords',
-            help='the keyword(s) to search for (note that these must be at the end of the argument list)',
-            nargs=argparse.REMAINDER)
+    parser.add_argument('-b', '--bin-size',
+            help='the number of days to group together as one datapoint. Higher number is more smooth graph, lower number is more spiky. Default 3')
+    required.add_argument('-f','--file',
+            help='path to the json file (chat log) to analyse')
+    required.add_argument('-k','--keywords',
+            help='the keyword(s) to search for',
+            nargs='+',
+            required = True)
 
     args = parser.parse_args()
-    filepath = args.filepath
+    filepath = args.file
     keywords = args.keywords
     savefolder = args.output_folder
+    binsize = 3
 
     with open(filepath, 'r') as jsonfile:
         events = (loads(line) for line in jsonfile)
         #generator, so whole file is not put in mem
         counters = [defaultdict(list) for keyword in keywords]
-        #list of defaultdicts. one dd per keyword
-        for event in events:
-            if "text" in event:
-            #if the event has the json tag "text" ie if it's a text message then search for given keywords. there is no else.
-                for keyword in keywords:
-                    if args.case_sensitive:
-                    #case sens or insens search for the keyword
-                        day = date.fromtimestamp(event["date"])
-                        counters[keywords.index(keyword)][day].append(
-                            "text" in event and keyword in event["text"])
-                    else:
-                        day = date.fromtimestamp(event["date"])
-                        counters[keywords.index(keyword)][day].append(
-                            "text" in event and keyword in event["text"].lower())
+
+        if binsize > 1:
+            for ind,event in enumerate(events):
+                if ind==0 or (curbin - date.fromtimestamp(event['date']) > timedelta(days=binsize)):
+                    curbin=date.fromtimestamp(event['date'])
+                if "text" in event:
+                    for keyword in keywords:
+                        if args.case_sensitive:
+                        #case sens or insens search for the keyword
+                            counters[keywords.index(keyword)][curbin].append(
+                                "text" in event and keyword in event["text"])
+                        else:
+                            counters[keywords.index(keyword)][curbin].append(
+                                "text" in event and keyword in event["text"].lower())
+        else:
+            for event in events:
+                if "text" in event:
+                    for keyword in keywords:
+                        if args.case_sensitive:
+                        #case sens or insens search for the keyword
+                            day = date.fromtimestamp(event["date"])
+                            counters[keywords.index(keyword)][day].append(
+                                "text" in event and keyword in event["text"])
+                        else:
+                            day = date.fromtimestamp(event["date"])
+                            counters[keywords.index(keyword)][day].append(
+                                "text" in event and keyword in event["text"].lower())
 
     _, filename = path.split(filepath)
     filename, _ = path.splitext(filename)
@@ -80,17 +98,14 @@ def main():
         plt.legend(keywords)
     else:
         plt.title(
-            "usage of {} in {}{}".format(keywords[0], filename, postfix))
+            'usage of "{}" in {}{}'.format(keywords[0], filename, postfix))
         #plt.legend(keywords[0])
 
 #    plt.ylabel("Percentage of messages containing \"{}\"".format(keyword), size=14)
     plt.ylabel("Percentage of messages containing keywords", size=14)
     if savefolder is not None:
     #if there is a given folder to save the figure in, save it there
-        keywords_string = ""
-        for keyword in keywords:
-            keywords_string+=keyword
-            keywords_string+="_"
+        keywords_string = '_'.join(keywords)
 
         if len(keywords_string) > 200:
         #file name likely to be so long as to cause issues
